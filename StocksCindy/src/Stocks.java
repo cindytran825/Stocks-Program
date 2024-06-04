fea89ed02b78987488eb72e68fa1220d30326cdf
cindytran825 <116225350+cindytran825@users.noreply.github.com>
2024-06-04 16:41:53 -0400

    Add files via upload

---
+++ StocksCindy/src/Stocks.java
@@ -0,0 +1,27 @@
+public interface Stocks {
+
+  //method that calculates the gain/loss
+  //specific period
+  public int gainLoss(String startDate, String endDate);
+
+  //getters that get the end and start dates
+  public String getEnd();
+
+  public String getStart();
+
+
+  //method that lets the person buy a stock ??
+  //adds stock to the portfolio
+  public Stocks addStock(String startDate, String endDate);
+
+
+
+
+  //method that takes in the dates and returns the moving average
+  //of the last x days
+  public Stocks movingAverage(int date1, int date2);
+
+
+
+
+}
---
+++ StocksCindy/src/StocksModel.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class StocksModel implements Stocks {
+  String date;
+
+  //exception that
+  //checks if the given date is within the given dates(2024-2014)
+
+
+  //this get the two dates and dates in between and puts them in list.
+  //return an array
+//  private String getDate(String date) {
+//    return null;
+//  }
+
+
+  @Override
+  public int gainLoss(String startDate, String endDate) {
+    return 0;
+  }
+  //
+  @Override
+  public String getEnd() {
+    return "";
+  }
+
+  @Override
+  public String getStart() {
+    return "";
+  }
+
+  @Override
+  public Stocks addStock(String startDate, String endDate) {
+    return null;
+  }
+
+  @Override
+  public Stocks movingAverage(int date1, int date2) {
+    return null;
+  }
+
+
+
+}
